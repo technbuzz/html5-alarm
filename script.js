0b1d3de071d6826216373516c1a86f8f507ec628
Sabeeh Javed <sabeehjaved1991@gmail.com>
2020-01-09 17:17:20 +0100

    adding selection

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const timeEl = document.querySelector('time');
 const audio = document.querySelector('audio');
 
+const alarmForm = document.querySelector('form');
+const dateEl = document.querySelector('form #datetime');
+alarmForm.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(dateEl.value)
+})
+
 const alarmTime = "1/9/2020 4:07:00 PM";
 function printTime(){
   const time = new Date();
