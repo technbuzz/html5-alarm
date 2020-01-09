62e632727ccb9a47c789007e1dcde6cbea2dffe9
Sabeeh Javed <sabeehjaved1991@gmail.com>
2020-01-09 16:05:27 +0100

    inital commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const timeEl = document.querySelector('time');
+const audio = document.querySelector('audio');
+
+const alarmTime = "1/9/2020 3:58:15 PM";
+function printTime(){
+  const time = new Date();
+  const timeString = `${time.getHours()} : ${time.getMinutes()} :   ${time.getSeconds()}`;
+  
+  
+  timeEl.textContent = timeString
+  
+  // Alarm detection
+  const now = `${time.toLocaleDateString()} ${time.toLocaleTimeString()}`
+  console.log('now', now)
+  if(Date.parse(now) === Date.parse(alarmTime)){
+    timeEl.classList.add('tada')
+    audio.play()
+    console.log('Alarm is ringing!!!!!!!!!!!!!!!!!!!!!!')
+  }
+}
+
+
+setInterval(()=> {
+  printTime()
+}, 1000)
+
+window.onload = func
